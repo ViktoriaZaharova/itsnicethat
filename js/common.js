3aff55fb2032bbea0b23b3545d38b5a492f56156
Viktoria <zaharova181114@gmail.com>
2021-06-12 07:04:19 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,4 @@
+$('.btn-menu').click(function () {
+    $(this).removeClass('mineshaft theme-l__mineshaft theme-d__mineshaft theme-d__touch-white theme-l__touch-mineshaft');
+    console.log(1);
+});
