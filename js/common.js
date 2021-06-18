93816d5ed56b9a8a56127a1bd5c88483a9abb035
Viktoria <zaharova181114@gmail.com>
2021-06-18 08:03:08 +0300

    edit

--- js/common.js
+++ js/common.js
@@ -1,4 +1,17 @@
 $('.btn-menu').click(function () {
-    $(this).removeClass('mineshaft theme-l__mineshaft theme-d__mineshaft theme-d__touch-white theme-l__touch-mineshaft');
-    console.log(1);
+    $(this).toggleClass('mineshaft').toggleClass('white').find('.icon-burger').toggleClass('icon-burger--is-active');
+    if($('.btn-menu').hasClass('white')) {
+        $(this).parents('header').find('.mineshaft').removeClass('mineshaft theme-d__white theme-l__mineshaft theme-d__touch-white theme-l__touch-mineshaft').addClass('white theme-d__white theme-l__white');
+    } else {
+        $(this).parents('header').find('.theme-d__white.theme-l__white').removeClass('white theme-d__white theme-l__white').addClass('mineshaft theme-d__white theme-l__mineshaft theme-d__touch-white theme-l__touch-mineshaft');
+    }
+    $('.accessible-hide-menu').toggleClass('accessible-hide');
+
 });
+
+$('.btn-category').click(function () {
+    console.log(1);
+    $(this).toggleClass('theme-d__white theme-d__touch-white theme-l__touch-mineshaft').toggleClass('theme-d__mineshaft hover-violet hover-bg-magnolia').find('.btn-category-icon').toggleClass('rotate');
+    $('.accessible-hide-category').toggleClass('accessible-hide');
+});
+
